2128b75913ac602724ba5b6673f708ebbb7a3fa1
Hugo Pochart <hugo.pochart@gmail.com>
2017-12-05 14:25:13 +0100

    add crypto controller

--- Gemfile
+++ Gemfile
@@ -12,6 +12,8 @@ gem 'redis'
 gem 'autoprefixer-rails'
 gem 'bootstrap-sass'
 gem 'font-awesome-sass'
+gem 'jquery-rails'
+
 gem 'sass-rails'
 gem 'simple_form'
 gem 'uglifier'
--- app/controllers/cryptos_controller.rb
+++ app/controllers/cryptos_controller.rb
@@ -2,8 +2,10 @@ require 'cryptocompare'
 
 class CryptosController < ApplicationController
   def index
+    @cryptos = Crypto.all
   end
 
   def show
+    @crypto = Crypto.find(params[:id])
   end
 end
