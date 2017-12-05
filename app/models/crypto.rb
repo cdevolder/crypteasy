bc896c8ac13969e65fe3912842604c6e90697eeb
Alexandre Dibon <alexandre.dibon@gmail.com>
2017-12-05 11:21:10 +0100

    add validations to models

--- app/models/crypto.rb
+++ app/models/crypto.rb
@@ -2,4 +2,9 @@ class Crypto < ApplicationRecord
 
   has_many :exchangetimevalues
   has_many :favorites
+
+  validates :name, presence: true, uniqueness: true
+  validates :accronym, presence: true
+
+
 end
