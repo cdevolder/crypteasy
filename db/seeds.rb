3cd46adb463f760a08396246a848a3fa4d2c9f84
Alexis Maerten <maerten.alexis@gmail.com>
2017-12-05 11:19:12 +0100

    seed pas fini

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,37 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+Platform.destroy_all
+Crypto.destroy_all
+
+
+platform = Platform.create!(
+  name: 'Kraken',
+  description: ''
+  )
+
+platform2 = Platform.create!(
+  name: 'Gdax',
+  description: ''
+  )
+
+platform3 = Platform.create!(
+  name: 'Bitstamp',
+  description: ''
+  )
+
+crypto = Crypto.create!(
+  name: 'Bitcoin',
+  accronym: 'BTC'
+  )
+
+crypto2 = Crypto.create!(
+  name: 'Ethereum',
+  accronym: 'ETH'
+  )
+crypto3 = Crypto.create!(
+  name: 'Litcoin',
+  accronym: 'LTC'
+  )
+
+
