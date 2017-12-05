316202623e550bba2bdfb43eb998cad9a805c9f9
Alexis Maerten <maerten.alexis@gmail.com>
2017-12-05 11:36:54 +0100

    seed solution facile

--- db/seeds.rb
+++ db/seeds.rb
@@ -39,8 +39,74 @@ crypto3 = Crypto.create!(
   )
 
 exchangetimevalues = Exchangetimevalues.create!(
-  euros: ,
-  dollars: ,
-  yuan:
+  euros: '1000' ,
+  dollars:'1000' ,
+  yuan: '1000',
+  platform: 'platform',
+  Crypto: 'crypto'
+  )
+
+exchangetimevalues2 = Exchangetimevalues.create!(
+  euros: '1500' ,
+  dollars:'1500' ,
+  yuan: '1500',
+  platform: 'platform',
+  Crypto: 'crypto2'
+  )
+
+exchangetimevalues3 = Exchangetimevalues.create!(
+  euros: '5000' ,
+  dollars:'5000' ,
+  yuan: '5000',
+  platform: 'platform',
+  Crypto: 'crypto3'
+  )
+
+exchangetimevalues4 = Exchangetimevalues.create!(
+  euros: '2000' ,
+  dollars:'2000' ,
+  yuan: '2000',
+  platform: 'platform2',
+  Crypto: 'crypto'
+  )
+
+exchangetimevalues5 = Exchangetimevalues.create!(
+  euros: '2500' ,
+  dollars:'2500' ,
+  yuan: '2500',
+  platform: 'platform2',
+  Crypto: 'crypto2'
+  )
+
+exchangetimevalues6 = Exchangetimevalues.create!(
+  euros: '10000' ,
+  dollars:'10000' ,
+  yuan: '10000',
+  platform: 'platform2',
+  Crypto: 'crypto3'
+  )
+
+exchangetimevalues7 = Exchangetimevalues.create!(
+  euros: '3000' ,
+  dollars:'3000' ,
+  yuan: '3000',
+  platform: 'platform3',
+  Crypto: 'crypto'
+  )
+
+exchangetimevalues8 = Exchangetimevalues.create!(
+  euros: '2500' ,
+  dollars:'2500' ,
+  yuan: '2500',
+  platform: 'platform3',
+  Crypto: 'crypto2'
+  )
+
+exchangetimevalues9 = Exchangetimevalues.create!(
+  euros: '15000' ,
+  dollars:'15000' ,
+  yuan: '15000',
+  platform: 'platform3',
+  Crypto: 'crypto3'
   )
 
