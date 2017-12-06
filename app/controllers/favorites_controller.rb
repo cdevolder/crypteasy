f08ff78bbc47d0560f0e689f33826109df4a0713
Hugo Pochart <hugo.pochart@gmail.com>
2017-12-06 19:48:09 +0100

    upload graph

--- app/controllers/favorites_controller.rb
+++ app/controllers/favorites_controller.rb
@@ -2,7 +2,26 @@ require 'cryptocompare'
 
 class FavoritesController < ApplicationController
   def index
-    @timevalues = Exchangetimevalue.where("platform_id = '1' AND crypto_id = '1'")
+    @timevalues = Exchangetimevalue.where("platform_id = '3' AND crypto_id = '1'")
+    @timevalues1 = Exchangetimevalue.where("platform_id = '2' AND crypto_id = '1'")
+    @alltimevalues = []
+    @alltimevalues1 = []
+    @min = @timevalues[0].euro.to_f
+    @max = @timevalues[0].euro.to_f
+    @timevalues.each_with_index do |timeval, ind|
+      @alltimevalues << [timeval.created_at.to_time.to_i,timeval.euro]
+      @alltimevalues1 << [@timevalues1[ind].created_at.to_time.to_i, @timevalues1[ind].euro]
+      # @alltimevalues << [ind+1,timeval.euro]
+      if timeval.euro < @min
+        @min = timeval.euro.to_f
+      end
+      if timeval.euro > @max
+        @max = timeval.euro.to_f
+      end
+      @min = @min
+      @max = @max
+
+    end
 
   end
 
