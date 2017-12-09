ba5bc21226f93e0b05a42d463ebc42589a1c8a16
Alexandre Dibon <alexandre.dibon@gmail.com>
2017-12-09 17:23:32 +0100

    fix

--- app/twilio/send_sms.rb
+++ app/twilio/send_sms.rb
@@ -1,7 +1,7 @@
 require 'twilio-ruby'
 
 account_sid = "AC20ce19122a60321c188fb3bfe4ec98b5" # Your Account SID from www.twilio.com/console
-auth_token = "f6eaca786614a05a18fb503c1793a224"   # Your Auth Token from www.twilio.com/console
+auth_token = "***"   # Your Auth Token from www.twilio.com/console
 
 begin
   @client = Twilio::REST::Client.new account_sid, auth_token
