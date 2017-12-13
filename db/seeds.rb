e3848034fa0416e2cf0c39b892b7f9dff78edd0c
Alexandre Dibon <alexandre.dibon@gmail.com>
2017-12-13 16:48:06 +0100

    ok

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,40 +7,6 @@
 #   Character.create(name: 'Luke', movie: movies.first)
 Offer.destroy_all
 
-
-
-platform = Platform.create!(
-  name: 'Kraken',
-  description: 'ok',
-  )
-
-platform2 = Platform.create!(
-  name: 'Gdax',
-  description: 'ok',
-  )
-
-platform3 = Platform.create!(
-  name: 'Bitstamp',
-  description: 'ok',
-  )
-
-crypto = Crypto.create!(
-  name: 'Bitcoin',
-  accronym: 'BTC',
-  photo_url: 'https://www.cryptocompare.com/media/19633/btc.png'
-  )
-
-crypto2 = Crypto.create!(
-  name: 'Ethereum',
-  accronym: 'ETH',
-  photo_url: 'https://www.cryptocompare.com/media/20646/eth_logo.png'
-  )
-crypto3 = Crypto.create!(
-  name: 'Litcoin',
-  accronym: 'LTC',
-  photo_url: 'https://www.cryptocompare.com/media/19782/litecoin-logo.png'
-  )
-
 offer1 = Offer.create!(
   sku: 'Bronze',
   name: 'Bronze',
