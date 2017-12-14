39ee567d24892e8dab8c310e72d4ed641ae2053b
Alexandre Dibon <alexandre.dibon@gmail.com>
2017-12-14 15:14:40 +0100

    ok

--- app/models/favorite.rb
+++ app/models/favorite.rb
@@ -2,4 +2,7 @@ class Favorite < ApplicationRecord
   belongs_to :user
   belongs_to :crypto
   has_many :alerts, dependent: :destroy
+
+  validates :platform1, presence: true
+  validates :platform2, presence: true
 end
